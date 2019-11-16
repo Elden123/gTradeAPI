33e527f93bd4d0b1d1159bafe8d421f341b4509a
Harrison Heeb <harrisonheeb@Harrisons-MacBook-Pro.local>
2019-11-16 16:46:17 -0600

    sentiment working now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let googleTrends = require('google-trends-api');
 let port = process.env.PORT || 3000;
 let app = express();
-var sentiment = require('Sentiment');
+var sentiment = require('./Sentiment');
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
@@ -31,26 +31,29 @@ app.get('/trends/:company', function (req, res) {
   let company=req.query.company;
   res.send(company+"\n");
   googleTrends.interestByRegion({keyword: company, startTime: new Date('2019-11-15'), endTime: new Date('2019-11-16'), geo: 'US'});
-  .then((results) => {
-    res.send(results);
-  })
-  .catch((err) => {
-    res.send(err);
-  })
+  // .then((results) => {
+  //   res.send(results);
+  // })
+  // .catch((err) => {
+  //   res.send(err);
+  // })
 });
 
-app.get('/sentiment', function (req, res) {
+app.get('/sentiment/:url/:company', function (req, res) {
   //res.send("All company trends");
-      // var result = [];
-      // sentiment.getSentiment('https://www.breitbart.com/clips/2019/11/03/trump-i-think-nancy-pelosi-has-lost-her-mind/', 'Nancy').then(analysisResults => {
-      //   result.push(analysisResults.result.sentiment.targets[0].label);
-      //   result.push(analysisResults.result.sentiment.targets[0].score);
-      //   res.send(results);
-      //   console.log(result);
-      // })
-      // .catch(err => {
-      //   console.log('error:', err);
-      // });
+      var result = [];
+      //'https://www.breitbart.com/clips/2019/11/03/trump-i-think-nancy-pelosi-has-lost-her-mind/', 'Nancy'
+      let company=req.query.company;
+      let url=req.query.url;
+      sentiment.getSentiment(url, company).then(analysisResults => {
+        result.push(analysisResults.result.sentiment.targets[0].label);
+        result.push(analysisResults.result.sentiment.targets[0].score);
+        res.send(result);
+        console.log(result);
+      })
+      .catch(err => {
+        console.log('error:', err);
+      });
 });
 
 app.listen(port, function () {
