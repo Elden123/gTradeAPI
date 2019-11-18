5ca0e9fe5910fac6ed98aa1fa16e8d763c2affc1
Dawson <dawsonfrick84@gmail.com>
2019-11-18 11:11:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -39,19 +39,19 @@ app.get('/trends/:company', function (req, res) {
   // })
 });
 
-app.get('/sentiment', function (req, res) {
-  //res.send("All company trends");
-      var result = [];
-      sentiment.getSentiment('https://www.breitbart.com/clips/2019/11/03/trump-i-think-nancy-pelosi-has-lost-her-mind/', 'Nancy').then(analysisResults => {
-        result.push(analysisResults.result.sentiment.targets[0].label);
-        result.push(analysisResults.result.sentiment.targets[0].score);
-        res.send(results);
-        console.log(result);
-      })
-      .catch(err => {
-        console.log('error:', err);
-      });
-});
+// app.get('/sentiment', function (req, res) {
+//   //res.send("All company trends");
+//       var result = [];
+//       sentiment.getSentiment('https://www.breitbart.com/clips/2019/11/03/trump-i-think-nancy-pelosi-has-lost-her-mind/', 'Nancy').then(analysisResults => {
+//         result.push(analysisResults.result.sentiment.targets[0].label);
+//         result.push(analysisResults.result.sentiment.targets[0].score);
+//         res.send(results);
+//         console.log(result);
+//       })
+//       .catch(err => {
+//         console.log('error:', err);
+//       });
+// });
 
 app.listen(port, function () {
     console.log('Example app listening on port !');
